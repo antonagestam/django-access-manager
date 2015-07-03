9eb9878af13afe8a0a93177eb8e705f897a3ad35
Anton Agestam <msn@antonagestam.se>
2015-07-03 17:58:14 +0200

    change test structure

---
+++ access_manager/tests/test_decorators.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+
+from access_manager.decorators import access_requirements
+
+from .utils import (
+    SuccessfulRequirement, UnSuccessfulRequirement, successful_response,
+    unsuccessful_response)
+
+
+def test_view(request, *args, **kwargs):
+    test_view.dispatch_called = True
+    return successful_response
+
+
+class TestAccessRequirementsDecorator(TestCase):
+    def setUp(self):
+        self.request = {}
+        test_view.dispatch_called = False
+
+    def test_successfull(self):
+        first = SuccessfulRequirement()
+        second = SuccessfulRequirement()
+        view = access_requirements(first, second)(test_view)
+        result = view(self.request)
+        self.assertTrue(test_view.dispatch_called)
+        self.assertEqual(result, successful_response)
+
+    def test_first_unfulfilled(self):
+        first = UnSuccessfulRequirement()
+        second = SuccessfulRequirement()
+        view = access_requirements(first, second)(test_view)
+        result = view(self.request)
+        self.assertFalse(test_view.dispatch_called)
+        self.assertEqual(result, unsuccessful_response)
+
+    def test_second_unfulfilled(self):
+        first = SuccessfulRequirement()
+        second = UnSuccessfulRequirement()
+        view = access_requirements(first, second)(test_view)
+        result = view(self.request)
+        self.assertFalse(test_view.dispatch_called)
+        self.assertEqual(result, unsuccessful_response)
